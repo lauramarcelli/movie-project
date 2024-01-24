4255b6a1d0f2514494894de9dd190df3fab9b301
lauramarcelli <laura.marcelli@gmail.com>
2024-01-24 11:01:12 -0300

    initializing function to show slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MovieSlider from "./components/MovieSlider";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
      <BrowserRouter>
         <Header />
+        <MovieSliderSlider images={movieImage} />
         <Routes>
           {/* <Route path="/" element={<ContainCard />} />
           <Route path="/detailCard/:id" element={<DetailCard />} /> */}
--- src/components/MovieSlider.jsx
+++ src/components/MovieSlider.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function MovieSlider() {
-  return (
-    <div>MovieSlider</div>
-  )
+  	// Variables y Estados
+	const [image, setImage] = useState([0]);
+	const quantity = images?.length;
+
+	// Return prematuro para evitar errores
+	if (!Array.isArray(images) || quantity === 0) return;
+
+	const nextImage = () => {
+		setActualImage(image === quantity - 1 ? 0 : image + 1);
+	};
+
+	const previousImage = () => {
+		setActualImage(image === 0 ? quantity - 1 : image - 1);
+	};
+
+	return (
+		<div className={estilos.container}>
+			<button onClick={previousImage}>←</button>
+			{images.map((imagen, index) => {
+				return (
+					<div
+						className={
+							image === index
+								? `${estilos.slide} ${estilos.active}`
+								: estilos.slide
+						}>
+						{image === index && (
+							<img key={index} src={imagen} alt="imagen" />
+						)}
+					</div>
+				);
+			})}
+			<button onClick={nextImage}>→</button>
+		</div>
+	);
 }
+  
+
