11fd8c68d183da6ad4675b86c4f7ce53b27a7708
lauramarcelli <laura.marcelli@gmail.com>
2024-01-28 14:26:23 -0300

    modifing fetch

--- src/hooks/useMovies.jsx
+++ src/hooks/useMovies.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 export default function useMovies() {
   
   const [data, setData] = useState([]);
+  const [mejorPuntuadas, setMejorPuntuadas] = useState([]);
   const apikey = import.meta.env.VITE_APP_MOVIE_KEY;
 
   const getPopular = async () => {
@@ -22,14 +23,14 @@ export default function useMovies() {
   const getTopRated = async () => {
     try {
       
-      const response = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apikey}`);
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apikey}&language=es-ES&page=1`);
 
-      console.log(response.data.results);
-      setData(response.data.results);
+      console.log(response.mejorPuntuadas.results);
+      setMejorPuntuadas(response.mejorPuntuadas.results);
     } catch (error) {
       console.log(error);
     }
   };
 
-  return { data, getPopular, getTopRated };
+  return { data, getPopular, mejorPuntuadas, getTopRated };
 }
