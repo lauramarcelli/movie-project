4e8237bf53213338afec1040cbee37ada3de789b
lauramarcelli <laura.marcelli@gmail.com>
2024-02-02 17:51:24 -0300

    adding searching funtion

--- src/hooks/useMovies.jsx
+++ src/hooks/useMovies.jsx
@@ -2,13 +2,11 @@ import axios from "axios";
 import { useState } from "react";
 
 export default function useMovies() {
-  
   const [data, setData] = useState([]);
   const apikey = import.meta.env.VITE_APP_MOVIE_KEY;
 
   const getMovies = async (category) => {
     try {
-      
       const response = await axios.get(
         `https://api.themoviedb.org/3/movie/${category}?api_key=${apikey}&language=es-ES&page=1`
       );
@@ -21,8 +19,9 @@ export default function useMovies() {
 
   const getMovie = async (id) => {
     try {
-      
-      const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/?api_key=${apikey}&language=es-ES&page=1`);
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${id}/?api_key=${apikey}&language=es-ES&page=1`
+      );
 
       console.log(response.data);
       setData(response.data);
@@ -31,37 +30,18 @@ export default function useMovies() {
     }
   };
 
-  return { data, getMovies, getMovie };
+  const searchMovie = async (nameMovie) => {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?query=${nameMovie}&api_key=${apikey}&language=es-ES&page=1`
+      );
+
+      setData(response.data.results);
+    } catch (error) {
+      console.log("Error fetching");
+    }
+  };
+
+  return { data, getMovies, getMovie, searchMovie };
 }
 
-  
-// const [popular, setPopular] = useState([]);
-// const [topRated, setTopRated] = useState([]);
-// const apikey = import.meta.env.VITE_APP_MOVIE_KEY;
-
-// const getPopular = async () => {
-//   try {
-    
-//     const response = await axios.get(
-//       `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=es-ES&page=1`
-//     );
-
-//     setPopular(response.data.results);
-//   } catch (error) {
-//     console.log("Error fetching");
-//   }
-// };
-
-// const getTopRated = async () => {
-//   try {
-    
-//     const response = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apikey}&language=es-ES&page=1`);
-
-//     console.log(response.data.results);
-//     setTopRated(response.data.results);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// return { popular, getPopular, topRated, getTopRated };
