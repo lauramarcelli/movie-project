c3e4325510a97e67e04425ef1c5299421260ab81
lauramarcelli <laura.marcelli@gmail.com>
2024-02-03 23:15:33 -0300

    beggining to work on favorites function

--- src/components/MovieCatalogue/MovieCatalogue.jsx
+++ src/components/MovieCatalogue/MovieCatalogue.jsx
@@ -3,7 +3,8 @@ import useMovies from "../../hooks/useMovies";
 import { useEffect } from "react";
 import { Col, Container, Card, Row } from "react-bootstrap";
 import { TfiEye } from "react-icons/tfi";
-import "./MovieCatalogue.css"
+import { FaRegHeart } from "react-icons/fa"
+import "./MovieCatalogue.css";
 
 export default function MovieCatalogue({ category }) {
   const { data, getMovies } = useMovies();
@@ -15,24 +16,34 @@ export default function MovieCatalogue({ category }) {
   return (
     <>
       <Container>
-      <Row>
-        {data.map((movie) => {
-          return (
-         <Col>
-              <Card key={movie.id} className="custom-card" style={{ width: "18rem" }}>
-                <Card.Img
-                  variant="top"
-                  src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-                />
-                <Card.Body>
-                  <Card.Title className="custom-title">{movie.title}</Card.Title>
-                  <Button variant="secondary"><TfiEye /></Button>
-                </Card.Body>
-              </Card>
+        <Row>
+          {data.map((movie) => {
+            return (
+              <Col>
+                <Card
+                  key={movie.id}
+                  className="custom-card"
+                  style={{ width: "18rem" }}
+                >
+                  <Card.Img
+                    variant="top"
+                    src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                  />
+                  <Card.Body>
+                    <Card.Title className="custom-title">
+                      {movie.title}
+                    </Card.Title>
+                    <Button className="custom-btn"variant="secondary">
+                      <TfiEye />
+                    </Button>
+                    <Button className="custom-btn"variant="secondary">
+                    <FaRegHeart />
+                    </Button>
+                  </Card.Body>
+                </Card>
               </Col>
-          );
-          
-        })}
+            );
+          })}
         </Row>
       </Container>
     </>
